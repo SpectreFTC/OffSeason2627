f05a096769fe48a9bb688737cfc91c206d217ce1
Minessh Uchat <minessh.uchat@gmail.com>
2026-08-10 19:11:18 -0500

    Fixed TeleOp with CommandScheduler

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
+import org.firstinspires.ftc.teamcode.StateMachine;
 import org.firstinspires.ftc.teamcode.commandbase.commands.Drive.DefaultDriveCommand;
 import org.firstinspires.ftc.teamcode.commandbase.commands.Intake.IntakeCommand;
 import org.firstinspires.ftc.teamcode.commandbase.commands.Intake.OuttakeCommand;
@@ -11,15 +16,25 @@ import org.firstinspires.ftc.teamcode.commandbase.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.commandbase.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.globals.RobotHardware;
 
-public class TeleOp extends CommandOpMode {
+public class TeleOp extends OpMode {
+
+    CommandScheduler commandScheduler = CommandScheduler.getInstance();
 
     private RobotHardware robot;
     private DriveSubsystem driveSubsystem;
     private IntakeSubsystem intakeSubsystem;
     private GamepadEx driverGamepad;
 
+    enum State {
+        INTAKE, OUTTAKE, IDLE
+    }
+
     @Override
-    public void initialize() {
+    public void init() {
+        commandScheduler.reset();
+
+        StateMachine.resetInstance();
+
         robot = RobotHardware.getInstance(this);
 
         driveSubsystem = new DriveSubsystem(robot.frontLeft, robot.frontRight, robot.backLeft, robot.backRight);
@@ -28,11 +43,24 @@ public class TeleOp extends CommandOpMode {
 
         driverGamepad = new GamepadEx(gamepad1);
 
-        driveSubsystem.setDefaultCommand(new DefaultDriveCommand(driveSubsystem, () -> driverGamepad.getLeftX(), () -> driverGamepad.getLeftY(), () -> driverGamepad.getRightX()));
+        StateMachine sm = StateMachine.builder()
+                .state(State.INTAKE, new IntakeCommand(intakeSubsystem))
+                .state(State.OUTTAKE, new OuttakeCommand(intakeSubsystem))
+                .state(State.IDLE, new InstantCommand())
+                .transition(State.INTAKE, State.OUTTAKE, () -> driverGamepad.getButton(GamepadKeys.Button.LEFT_BUMPER))
+                .transition(State.OUTTAKE, State.INTAKE, () -> driverGamepad.getButton(GamepadKeys.Button.RIGHT_BUMPER))
+                .anyTransition(State.IDLE, () -> driverGamepad.getButton(GamepadKeys.Button.A))
+                .build();
 
-        driverGamepad.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whileHeld(new IntakeCommand(intakeSubsystem));
+        commandScheduler.schedule(sm);
+    }
 
-        driverGamepad.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whileHeld(new OuttakeCommand(intakeSubsystem));
+    @Override
+    public void loop() {
+
+        commandScheduler.run();
+
+        driveSubsystem.driveRobotCentric(driverGamepad.getLeftX(), driverGamepad.getLeftY(), driverGamepad.getRightX());
 
     }
 }
